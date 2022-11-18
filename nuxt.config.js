ade30f2543dc57063999921a5a25cf81a677732a
Dawid <dawstask@gmail.com>
2022-11-18 22:06:44 +0100

    trying to fix problem with statics

--- nuxt.config.js
+++ nuxt.config.js
@@ -49,9 +49,6 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {},
-  router: {
-    base: '/uxit/'
-  },
 
   // Allows page refresh to work on github pages
   generate: {
